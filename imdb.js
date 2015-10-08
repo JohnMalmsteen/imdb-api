7c6b9e3b95d46728fb4e3a5d03cf7874098cbdb0
JohnMalmsteen <g00305339@gmit.ie>
2015-10-08 13:05:16 +0100

    Excercises Complete

--- imdb.js
+++ imdb.js
@@ -19,5 +19,32 @@ app.get('/movie/id/:id', function(req, res) {
   res.json(data[req.params.id]);
 });
 
+app.get('/movie/random', function(req, res){
+  if(req.query.nomovies == null){
+    res.json(data[Math.floor(Math.random() * data.length)]);
+  }
+  else {
+    var randommovies = [];
+
+    for(i = 0; i < req.query.nomovies; i++){
+      randommovies.push(data[Math.floor(Math.random() * data.length)]);
+    }
+
+    res.json(randommovies);
+  }
+});
+
+app.get('/movie/search/:term', function(req, res){
+  var results = [];
+  data.forEach(function(entry){
+    if(entry.title.search(req.params.term) != -1){
+      results.push(entry);
+    }
+  });
+
+  res.json(results);
+});
+
+
 // Start the server.
 var server = app.listen(8000);
